550b3d9bb48ceb79d7baeba6b83ef4e82281e420
Al Nguyen <al.nguyen@gmail.com>
2012-12-15 13:28:56 -0500

    Refactor Fabric files

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -1,12 +1,41 @@
+
 from __future__ import with_statement
+
+import os
+import sys
+from getpass import getuser
+
 from fabric.api import cd
 from fabric.api import env
 from fabric.api import local
 from fabric.api import run
 from fabric.api import settings
 
-env.hosts = ['nsquareu@nsquare.us']
 
+################
+# Config setup #
+################
+
+conf = {}
+if sys.argv[0].split(os.sep)[-1] == "fab":
+    # Ensure we import settings from the current dir
+    try:
+        conf = __import__("settings", globals(), locals(), [], 0).FABRIC
+        try:
+            conf["HOSTS"][0]
+        except (KeyError, ValueError):
+            raise ImportError
+    except (ImportError, AttributeError):
+        print "Aborting, no hosts defined."
+        exit()
+
+env.user = conf.get("SSH_USER", getuser())
+env.hosts = conf.get("HOSTS", [])
+
+
+##############
+# Deployment #
+##############
 
 def deploy():
     code_dir = '~/www/missionchinesefood'
